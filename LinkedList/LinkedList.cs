4c6d109fe588ddbe6bc26a06b884b6a05c293a1c
Chetan Deore <deorec346@gmail.com>
2022-04-25 08:46:02 +0530

    LinkedList

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -6,46 +6,44 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    namespace LinkedList
+
+    class LinkedList
     {
-        class LinkedList
+        //Instance
+        internal Node head;
+
+        //Appending Data to LinkedList
+        internal void Append(int data)
         {
-            //Instance
-            internal Node head;
+            //Object of Node
+            Node node = new Node(data);
 
-            //Adding Data to LinkedList
-            internal void Add(int data)
+            //Add Data
+            if (this.head == null)
+                this.head = node;
+            else
             {
-                //Object of Node
-                Node node = new Node(data);
-
-                //Add Data
-                if (this.head == null)
-                    this.head = node;
-                else
+                Node temp = head;
+                while (temp.next != null)
                 {
-                    Node temp = head;
-                    while (temp.next != null)
-                    {
-                        temp = temp.next;
-                    }
-                    temp.next = node;
+                    temp = temp.next;
                 }
+                temp.next = node;
             }
-            //Display LinkedList Data through Method
-            internal void Display()
+        }
+        //Display LinkedList Data through Method
+        internal void Display()
+        {
+            //Head is Null then List is empty otherwise print All data of Linked List
+            if (this.head == null)
+                Console.WriteLine("The list is empty.");
+            else
             {
-                //Head is Null then List is empty otherwise print All data of Linked List
-                if (this.head == null)
-                    Console.WriteLine("The list is empty.");
-                else
+                Node temp = head;
+                while (temp != null)
                 {
-                    Node temp = head;
-                    while (temp != null)
-                    {
-                        Console.WriteLine("Data : " + temp.data);
-                        temp = temp.next;
-                    }
+                    Console.WriteLine("Data : " + temp.data);
+                    temp = temp.next;
                 }
             }
         }
