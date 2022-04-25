4aff2725622b33637fd5eea2ac512e63bb79d086
Chetan Deore <97680266+deorec346@users.noreply.github.com>
2022-04-25 09:57:17 +0530

    Update Program.cs

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,58 +10,30 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            //Welcome Message
+           //Welcome Message
             Console.WriteLine("=-=-=-=-=-Welcome To The Linked List Program-=-=-=-=-=");
-            Console.WriteLine("-----------------------------------------------");
 
-            //Object of Class Sorte LinkedList
-            SortedLinkedList sortedLinkedList = new SortedLinkedList();
-            sortedLinkedList.Add(56);
-            sortedLinkedList.Add(30);
-            sortedLinkedList.Add(40);
-            sortedLinkedList.Add(70);
-
-            Console.WriteLine("Ordered Linked List in Ascending Order ::");
-            Console.WriteLine("-----------------------------------------");
-
-            sortedLinkedList.Display();
-            /*//Calling Class LinkedList
+            //Calling Class LinkedList
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
-            //Searching an Element            
-            linkedList.SearchElement(30);            
-            
-            //Adding Data After 30
-            linkedList.InsertAtParticularPosition(3, 40);
-            
-            *//* //Display Data Before Delete Last element 
-             Console.WriteLine("-----------------------------------------------");
-             Console.WriteLine("Display Data Before Delete Last element ::");
-             Console.WriteLine("-----------------------------------------------");
-             linkedList.Display();
-             //Delete Last Element
-             linkedList.PopLastElement();
-            *//*
-             //Display Data After Add
-             Console.WriteLine("-----------------------------------------------");
-             Console.WriteLine("Display After Adding element ::");
-             Console.WriteLine("-----------------------------------------------");
-            
-             Console.WriteLine("Data In Linked List");
-             linkedList.Display();
-            //Delete element and then get size
-            linkedList.Delete(40);
-            
-            //Display Data After Add
-            Console.WriteLine("-----------------------------------------------");            
-            Console.WriteLine("Display After Delete element ::");
-            Console.WriteLine("Size of Linked List : " + linkedList.Size());
+
+            //Display Data Before Adding into  Linked List
             Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("Data In Linked List");
+            Console.WriteLine("Display Data Before Adding into  Linked List ::");
+            Console.WriteLine("-----------------------------------------------");
             linkedList.Display();
-*/
+
+            //Adding Data Between 2 Data
+            linkedList.InsertAtParticularPosition(2, 30);
+
+            //Display Data After Adding into  Linked List
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Display Data Before Adding into  Linked List ::");
+            Console.WriteLine("-----------------------------------------------");
+
+            linkedList.Display();
+
             Console.ReadLine();
         }
     }
